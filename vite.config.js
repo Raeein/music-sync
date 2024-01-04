342b61dc5f0a29af94855828985bab613f50f9dc
Raeein Bagheri <raeeinbagheri80@gmail.com>
2024-01-03 23:02:09 -0500

    add dockerfile and fix packages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import adapter from "@sveltejs/adapter-node";
 import { sveltekit } from '@sveltejs/kit/vite';
 
-/** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [adapter(), sveltekit()],
+    preview: {
+        port: 3000
+    },
+    server: {
+        port: 3000
+    }
 };
 
 export default config;
