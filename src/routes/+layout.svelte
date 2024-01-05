e0a52030e3a79b1ccd419fc7ace116f8d69de4b4
Raeein Bagheri <raeeinbagheri80@gmail.com>
2024-01-04 21:27:14 -0500

    wee

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import '../styles/global.css'
+</script>
+<header>
+    <p>Hi i am the head</p>
+</header>
+<main>
+    <slot></slot>
+</main>
+
+
+<footer>hi im a footer</footer>
+
+<style>
+    header {
+        display: flex;
+        justify-content: center;
+    }
+    main {
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    footer {
+        text-align: center;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import StreamingService from '/src/lib/streaming-service.svelte'
+    let name: string = "boo";
+    let isAuthenticatedSpotify: boolean = false;
+    let isAuthenticatedAppleMusic: boolean = false;
+
+    const handleClick = () => {
+        name = "jason";
+    }
+</script>
+
+<h1  on:click={handleClick}>Welcome to SvelteKit {name}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if isAuthenticatedSpotify}
+   <p>Spotify is Authenticated</p> 
+{/if}
+
+<StreamingService svgName="spotify-logo.svg"></StreamingService>
+<StreamingService svgName="apple-music.svg"></StreamingService>
+<style>
+    h1 {
+        background-color: red;
+    }
+</style>
